931eebece840ef5c37e0bd5d062a3f7c3d1af983
Andrew McInally <realfunnyestate@yahoo.com>
2022-03-01 17:38:24 -0700

    fixed app

--- src/App.js
+++ src/App.js
@@ -1,16 +1,34 @@
 import Counter from "./Counter";
 // this is a functional component
+import List from "./List";
 
 //returns html
 
 //you can think of components as partials
 
 const App = () => {
+  const myFaveFoods = ['a', 'b', 'c', 'd', 'e', 'f',];
+  const myFaveCryptos = ['a', 'b', 'c', 'd', 'e', 'f',];
+  const msg1 = 'my favorite foods are';
+  const msg2 = 'my favorite foods are';
+
+  
   return (
+    //jsx fragment
+    // in react the html element can only have one parent element
     <div>
       <Counter />
+      <List 
+        message={msg1}
+        items={myFaveFoods}
+      
+      />
       <Counter />
-      <Counter />
+      <List 
+        message={msg2}
+        items={myFaveCryptos}
+      
+      />
     </div>
   );
 };
--- src/Counter.js
+++ src/Counter.js
@@ -1,5 +1,26 @@
-const Counter = () => {
-  return <h1>Counter Component</h1>;
+//useState is a build in function on react
+import { useState } from "react";
+
+export const Counter = () => {
+  //convention is to add 'set' infront on the the first value in the array
+  const [count, setCount] = useState(0);
+
+  // a component must return html at the end
+  return (
+    <div className="we use className instead of class in react">
+      <p
+        //if using style and css property has a dash, it is now camelCase
+        style={{
+          color: count % 2 === 0 ? "blue" : "red",
+          fontSize: "100px",
+        }}
+      >
+        Count: {count}
+      </p>
+      <button onClick={() => setCount(count + 1)}>Increment</button>
+      <button onClick={() => setCount(count - 1)}>Decrement</button>
+    </div>
+  );
 };
 
 export default Counter;
